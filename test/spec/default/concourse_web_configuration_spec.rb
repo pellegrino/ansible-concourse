7e99abd266afd07ff2870aee4add9b4ae08edd5e
Vitor Pellegrino <vitor@pellegrino.io>
2021-05-20 12:40:48 +0000

    feat: adds prometheus monitoring by default
    This configures prometheus to bind to the local ip, and expose metrics on port 9090 by default

--- test/spec/default/concourse_web_configuration_spec.rb
+++ test/spec/default/concourse_web_configuration_spec.rb
@@ -3,4 +3,7 @@ require 'spec_helper'
 describe file('/opt/concourse/bin/concourse-web') do
   its(:content) { should contain '--vault-url' }
   its(:content) { should contain '--vault-client-token' }
+
+  its(:content) { should contain '--prometheus-bind-ip' }
+  its(:content) { should contain '--prometheus-bind-port' }
 end
