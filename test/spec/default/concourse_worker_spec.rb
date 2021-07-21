0cdf48c8a6155cf1dcc1f634a095ea794f1b7594
Vitor Pellegrino <vitor@pellegrino.io>
2021-07-21 10:24:34 +0000

    Configures the containerd-allow-host-access flag.
    Set to false by default

--- test/spec/default/concourse_worker_spec.rb
+++ test/spec/default/concourse_worker_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe file('/opt/concourse/bin/concourse-worker') do
   its(:content) { should contain '--runtime "containerd" ' }
   its(:content) { should contain '--containerd-dns-server "127.0.0.1" ' }
+  its(:content) { should contain '--containerd-allow-host-access "false"' }
 end
