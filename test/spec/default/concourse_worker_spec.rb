c6b460877be0d90ea08a2ce296aab41c5960fa3e
Vitor Pellegrino <vitor@pellegrino.io>
2021-07-22 05:15:18 +0000

    Favors env variable over config flag for host access
    It looks like the concourse allow-host-access flag is not being understood by the concourse worker binary

--- test/spec/default/concourse_worker_spec.rb
+++ test/spec/default/concourse_worker_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe file('/opt/concourse/bin/concourse-worker') do
   its(:content) { should contain '--runtime "containerd" ' }
+  its(:content) { should contain 'export CONCOURSE_CONTAINERD_ALLOW_HOST_ACCESS="false"' }
   its(:content) { should contain '--containerd-dns-server "127.0.0.1" ' }
-  its(:content) { should contain '--containerd-allow-host-access "false"' }
+  its(:content) { should_not contain '--containerd-allow-host-access "false"' }
 end
